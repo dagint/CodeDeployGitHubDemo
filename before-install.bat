e3c3cb0697f8fc8a0261a65b51f2117cdbbb734d
dagint <dagint@gmail.com>
2016-01-20 16:14:08 -0500

    added index.html delete

--- before-install.bat
+++ before-install.bat
@@ -1,3 +1,4 @@
 REM Install Internet Information Server (IIS). 
+del /q c:\inetpub\wwwroot\index.html
 c:\Windows\Sysnative\WindowsPowerShell\v1.0\powershell.exe -Command Import-Module -Name ServerManager
 c:\Windows\Sysnative\WindowsPowerShell\v1.0\powershell.exe -Command Install-WindowsFeature Web-Server
